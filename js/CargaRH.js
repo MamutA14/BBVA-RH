fa11083764150cc9f0286302a7925c0f592fcf44
Set Reyes <sreyes@minsait.com>
2022-09-06 11:51:36 -0500

    Creando la tabla de RH

---
+++ js/CargaRH.js
@@ -0,0 +1,28 @@
+const $contenido = document.getElementById("id-contenido");
+
+$contenido.appendChild(Tabla({
+    "cabecera": ["Nombre", "Posicion", "Entrevistador", "Estatus", "Fecha Entrevista"]
+}));
+
+
+function Tabla(AJson){
+    const {cabecera} = AJson;
+    const $tabla = document.createElement("table");
+    const $tcabecera = document.createElement("thead");
+    const $tcuerpo = document.createElement("tbody");
+
+    $tabla.className = "";
+
+    const $titulos = document.createElement("tr");
+    cabecera.forEach(titulo => {
+        const $titulo = document.createElement("th");
+        $titulo.innerText = titulo;
+        $titulos.appendChild($titulo);
+    });
+    $tcabecera.appendChild($titulos);
+    
+
+    $tabla.appendChild($tcabecera);
+    $tabla.appendChild($tcuerpo);
+    return $tabla;
+}
