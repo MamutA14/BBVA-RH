c68e9400baad32a336a311dd526c70feddf17451
Set Reyes <sreyes@minsait.com>
2022-09-06 10:46:54 -0500

    moduloRH

---
+++ js/Ingresar.js
@@ -0,0 +1,3 @@
+export function Ingresar(e){
+    console.log(e.target)
+}
---
+++ js/Login.js
@@ -0,0 +1,24 @@
+document.getElementById("btn-ingresar").addEventListener("click", (e)=>{
+    const user = document.getElementById("id-usuario").value;
+    const pwd = document.getElementById("id-password").value;
+
+    if(user === "" || pwd === ""){
+        alert("ingresa un usuario o contraseña");
+    } else {
+        switch(user && pwd){
+            case "superUsuario":
+                alert("existe");
+                break;
+                case "recursosHumanos":
+                    alert("existe");
+                    document.getElementById("id-formulario").setAttribute("action","/DesktopRH.html")
+                break;
+            case "entrevistador":
+                alert("existe");
+                break;
+            default:
+                alert("No existe el usuario o la contraseña es incorrecta");
+                break;
+        }
+    }
+})
