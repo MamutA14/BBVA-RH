3bf789e9eda58075537ec9861ec2b166c32c4751
Set Reyes <sreyes@minsait.com>
2022-09-08 08:07:34 -0500

    agregando contenido a SU

--- js/Login.js
+++ js/Login.js
@@ -8,13 +8,15 @@ document.getElementById("btn-ingresar").addEventListener("click", (e)=>{
         switch(user && pwd){
             case "superUsuario":
                 alert("existe");
+                document.getElementById("id-formulario").setAttribute("action","/DesktopSuperUsuario.html")
                 break;
-                case "recursosHumanos":
-                    alert("existe");
-                    document.getElementById("id-formulario").setAttribute("action","/DesktopRH.html")
+            case "recursosHumanos":
+                alert("existe");
+                document.getElementById("id-formulario").setAttribute("action","/DesktopRH.html")
                 break;
             case "entrevistador":
                 alert("existe");
+                document.getElementById("id-formulario").setAttribute("action","/DesktopEntrevistador.html")
                 break;
             default:
                 alert("No existe el usuario o la contraseña es incorrecta");
