3ec01515d7f1824f9a7694dcea7daa6c3ddce02a
MamutA14 <alam@ciencias.unam.mx>
2022-09-14 13:58:18 -0500

    Se subieron cambios

--- js/entrevistadorSes.js
+++ js/entrevistadorSes.js
@@ -15,6 +15,17 @@ if (user_local != "" &&  pwd_local != "" &&  user_local != null && pwd_local != 
         case "59590-4157":
             break;    
     }
+        //Extraigo los datos del local y así extraigo el nombre
+        let user_data = JSON.parse(localStorage.getItem('data_user'));
+        document.getElementById('name-user').innerHTML = `¡Bienvenido ${user_data['name']}!`
 }else{
     window.location = "/Login.html";
 }
+
+//Salir de la sesión
+document.getElementById("salir").addEventListener("click", () =>{
+    localStorage.removeItem('pwd');
+    localStorage.removeItem('user');
+    localStorage.removeItem('data_user');
+    window.location = "/Login.html";
+});
