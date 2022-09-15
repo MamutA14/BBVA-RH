fa5a01d0155fe424dffbc51039e5b04c8a776cb1
Marcos Bautista <marcosbr.2025@gmail.com>
2022-09-15 12:53:15 -0500

    superusuario

--- js/app-SuperUsuario.js
+++ js/app-SuperUsuario.js
@@ -6,8 +6,13 @@ fetch('https://jsonplaceholder.typicode.com/users')
  function cargarTabla(superUsuario){
 
     const contenido = document.querySelector('.tabla');
+    const contenidoRH = document.querySelector('.tabla-2');
     const buscar = document.querySelector('#search');
-
+    const buscarRH = document.querySelector('#searchRH');
+    if(contenido !== null && buscar !== null){
+    console.log(contenido);
+    console.log(buscar);
+    
 const filtrar = () =>{
 
 
@@ -45,7 +50,13 @@ const filtrar = () =>{
                 <td>${usuario.website}</td>
                 <td>${usuario.email}</td>
                 <td>${usuario.address.city}</td>
-                <td class="opcion"><select name="Seleccione"><i class="fa-sharp fa-solid fa-caret-down"></i></select></td> 
+                <td class="opcion">
+                    <select name="Seleccione" id="opciones">
+                        <option value="" selected>Seleccione</option>
+                        <option value="">Editar</option>
+                        <option value="">Eliminar</option>
+                    </select>
+                 </td>  
             </tr>
     
             `;
@@ -57,8 +68,56 @@ const filtrar = () =>{
   console.log(superUsuario);
     
 }
-
-    filtrar();
+  filtrar();
+    
     buscar.addEventListener('keyup',filtrar);
 
-  }
+}else{
+
+
+const filtrarRH = () =>{
+
+
+    let html = `<table>
+    <thead>
+        <tr>
+            <th>Nombre <i class="fa-sharp fa-solid fa-caret-down"></i></th>
+            <th>Posicion <i class="fa-sharp fa-solid fa-caret-down"></i></th>
+            <th>Registro <i class="fa-sharp fa-solid fa-caret-down"></i></th>
+            <th>Estatus <i class="fa-sharp fa-solid fa-caret-down"></i></th>
+            <th> <i class="fa-sharp fa-solid fa-caret-down"></i></th>
+        </tr>
+    </thead>
+    <tbody>   
+    `;
+
+
+    const textoRH = buscarRH.value.toLowerCase();
+   
+    for (const usuario of superUsuario) {
+       
+        let nombre = usuario.name.toLowerCase();
+        
+        if(nombre.indexOf(textoRH) !== -1){
+            html += `
+            <tr>
+                <td>${usuario.name}</td>
+                <td>${usuario.address.zipcode}</td>
+                <td>${usuario.username}</td>
+                <td>${usuario.company.name}</td>
+                <td><button  id = "btn-perfil" class="fa-solid fa-eye">  Ver Perfil</button></td>                         
+            </tr>
+    
+            `;
+        }
+    }
+    html += `</tbody>
+    </table>`; 
+    contenidoRH.innerHTML = html;
+  console.log(superUsuario);
+    
+}
+filtrarRH();
+buscarRH.addEventListener('keyup',filtrarRH);
+}
+ }
